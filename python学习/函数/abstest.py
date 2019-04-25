b8ccaceb68c9150fe2872691cef13bb3f23d2bcf
liujiyangyang <1318335493@qq.com>
2019-04-25 10:09:57 +0800

    定义函数

---
+++ python学习/函数/abstest.py
@@ -0,0 +1,14 @@
+# 如果你已经把my_abs()的函数定义保存为abstest.py文件了，那么，可以在该文件的当前目录下启动Python解释器，用from abstest import my_abs来导入my_abs()函数，注意abstest是文件名（不含.py扩展名）
+
+def my_abs(x):
+    if x>=0:
+        return x
+    else:
+        return -x
+my_abs(-9)
+
+
+
+
+#   问题
+# 如果你已经把my_abs()的函数定义保存为abstest.py文件了，那么，可以在该文件的当前目录下启动Python解释器，用from abstest import my_abs来导入my_abs()函数，注意abstest是文件名（不含.py扩展名
