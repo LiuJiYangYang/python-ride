c8050aaa6abcd152b0da65be4a851ca961a00957
liujiyangyang <1318335493@qq.com>
2019-04-30 17:09:05 +0800

    继承和多态

--- python学习/面向对象编程/类和实例.py
+++ python学习/面向对象编程/类和实例.py
@@ -3,35 +3,136 @@ class Student(object):
     pass
 
 
+# 定义Student类，根据Student类创建出Student的实例，创建实例是通过类名+（）实现
+bart=Student
+print(bart)      # 变量bart指向的就是一个Student的实例,后面的0x10a67a590是内存地址
+print(Student)
+
+bart.name='Bart Simpson'
+print(bart.name)           #  bart一个name属性
+
+# 类可以起到模板的作用，可以在创建实例，可以绑定的属性强制填写进去
+# 通过定义一个特殊的__init__方法，创建时绑定name,score等属性
+class Student(object):
+    def __init__(self,name,score):
+        self.name=name
+        self.score=score
+# 注意到__init__方法的第一个参数永远是self
+
+bart=Student('Bart Simpson',59)
+print(bart.name)
+print(bart.score)
+
+# 数据封装
+def print_score(std):
+    print('%s:%s'%(std.name,std.score))
+print_score(bart)
 
 
 
 
+class Student(object):
+    def __init__(self,name,score):
+        self.name=name
+        self.score=score
+    def print_score(self):
+        print('%s:%s'%(self.name,self.score))
+bart=Student('Bart Simpson',59)
+print(bart.name)
+print(bart.score)
+print(bart.print_score())
+
+
+class Student(object):
+    def get_garde(self):
+        if self.score>=90:
+            return 'A'
+        elif self.score>=60:
+            return 'B'
+        else:
+            return 'C'
+
+
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
+lisa = Student('Lisa', 99)
+bart = Student('Bart', 59)
+print(lisa.name, lisa.get_grade())
+print(bart.name, bart.get_grade())
+
+
+bart = Student('Bart Simpson', 59)
+lisa = Student('Lisa Simpson', 87)
+bart.age = 8
+print(bart.age)
+# lisa.age
+# print(lisa.age)
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# AttributeError: 'Student' object has no attribute 'age'
+
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Student(object):
+
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    def print_score(self):
+        print('%s: %s' % (self.name, self.score))
+
+    def get_grade(self):
+        if self.score >= 90:
+            return 'A'
+        elif self.score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
+bart = Student('Bart Simpson', 59)
+lisa = Student('Lisa Simpson', 87)
+
+print('bart.name =', bart.name)
+print('bart.score =', bart.score)
+bart.print_score()
+
+print('grade of Bart:', bart.get_grade())
+print('grade of Lisa:', lisa.get_grade())
 
 
 
+def print_score(self):
+    print('姓名：%s 分数：%d'%(self.name,self.score))
+def get_grade(self):
+    if self.score>=90:
+        return 'A'
+    elif self.score>=60:
+        return 'B'
+    else:
+        return 'C'
+# 创建类的实例
+student_a = Student('tangchunli', 100)
+student_b = Student('zhangsanfe', 60)
 
+student_a.print_score()
+print(student_a.name,student_a.score, student_a.get_grade())
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+student_b.print_score()
+print(student_b.name, student_b.score, student_b.get_grade())
 
 
 
---
+++ python学习/面向对象编程/继承和多态.py
@@ -0,0 +1,123 @@
+# 可以从某个现有的class继承，新的class称为子类Subclass，
+# 而被继承的class称为基类、父类或超类(Base class、Super class)
+
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+
+# 编写Dog和Cat类     从Animal类继承
+class Dog(Animal):
+    pass
+class Cat(Animal):
+    pass
+
+# Cat和Dog是Animal的子类
+dog=Dog()
+dog.run()
+
+cat=Cat()
+cat.run()
+
+# Animal is running...         运行结果
+# Animal is running...
+
+class Dog(Animal):
+    def run(self):
+        print('Dog is running...')
+    def eat(self):
+        print('Eating meat...')
+
+
+class Dog(Animal):
+    def run(self):
+        print('Dog is running...')
+class Cat(Animal):
+    def run(self):
+        print('Cat is running...')
+
+# 子类的run（）覆盖了父类的run(),在代码运行的时候，总是会调用子类的run()，我们就获得了继承的另一个好处：多态
+a = list() # a是list类型
+b = Animal() # b是Animal类型
+c = Dog() # c是Dog类型
+
+# 判断一个变量是否是某个类型可以用isinstance()判断
+isinstance(a,list)
+isinstance(b,Animal)
+isinstance(c,Dog)
+
+isinstance(c,Animal)    # c不仅仅是Dog，c还是Animal
+
+b=Animal()
+isinstance(b,Dog)       # Dog可以看成Animal，但Animal不可以看成Dog
+
+def run_twice(animal):
+    animal.run()
+    animal.run()
+
+run_twice(Animal())     # 打印出来
+
+run_twice(Dog())
+
+run_twice(Cat())
+
+# 定义一个Tortoise类型，也从Animal派生
+class Tortoise(Animal):
+    def run(self):
+        print('Tortoise is running slowly...')
+
+print(run_twice(Tortoise()))
+
+
+class Timer(object):
+    def run(self):
+        print('Start...')
+# 动态语言的“鸭子类型”，它并不要求严格的继承体系
+
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Animal(object):
+    def run(self):
+        print('Animal is running...')
+
+class Dog(Animal):
+    def run(self):
+        print('Dog is running...')
+
+class Cat(Animal):
+    def run(self):
+        print('Cat is running...')
+
+def run_twice(animal):
+    animal.run()
+    animal.run()
+
+a = Animal()
+d = Dog()
+c = Cat()
+
+print('a is Animal?', isinstance(a, Animal))
+print('a is Dog?', isinstance(a, Dog))
+print('a is Cat?', isinstance(a, Cat))
+
+print('d is Animal?', isinstance(d, Animal))
+print('d is Dog?', isinstance(d, Dog))
+print('d is Cat?', isinstance(d, Cat))
+
+run_twice(c)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
