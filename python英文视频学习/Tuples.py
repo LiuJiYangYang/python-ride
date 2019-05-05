1317c59a7c59b5bad6fe4bbf180f3af32a4aa3ed
liujiyangyang <1318335493@qq.com>
2019-05-05 22:48:42 +0800

    tuples

---
+++ python英文视频学习/Tuples.py
@@ -0,0 +1,20 @@
+# tuples是一种数据类型的结构
+coordinates=(4,5)  # 坐标
+# coordinates[1]=10      # tuples是不可变的
+print(coordinates[1])
+
+# lists可变的
+coordinates=[(4,5),(3.5),(8,9)]
+coordinates[1]=(42,4)
+print(coordinates)
+
+
+# tuples中的lists可变
+coordinates=(1,3,[3,4,5])
+coordinates[2][0]=7
+print(coordinates)
+
+
+
+
+
