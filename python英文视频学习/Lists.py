35fdebb1a6ef0e44ea91c63d3a93a3b88d8d57b4
liujiyangyang <1318335493@qq.com>
2019-05-05 22:04:52 +0800

    lists

---
+++ python英文视频学习/Lists.py
@@ -0,0 +1,13 @@
+# 储存数据的列表
+friends=["Kevin","Karen","Jim","Oscar","Toby"]     # 数字，字符串，False，布尔值
+print(friends)
+# 索引从0开始
+print(friends[0])
+print(friends[-1])
+# 选择从什么开始
+print(friends[1:])
+# 抓取元素
+print(friends[1:4])
+# 修改元素
+friends[1]="Mike"
+print(friends)
--- python英文视频学习/Mad Libs Game.py
+++ python英文视频学习/Mad Libs Game.py
@@ -1,7 +1,17 @@
 # 填空游戏
-print("Roses are {color}")   # 颜色
-print("{plural noun} are blue")   # 输入复数名词
-print("I love {celebrity}")    # 一个名人的回答
+color=input("Enter a color: ")   # Magente
+plural_noun=input("Enter a plural noun: ")   # Microwaves
+celebrity=input("Enter a celebrity: ")    # Tom Hanks
+
+
+# print("Roses are {color}")   # 颜色
+# print("{plural noun} are blue")   # 输入复数名词
+# print("I love {celebrity}")    # 一个名人的回答
+
+# 将变量加进去
+print("Roses are "+ color)
+print(plural_noun + "are blue")
+print("I love " + celebrity)
 
 
 
