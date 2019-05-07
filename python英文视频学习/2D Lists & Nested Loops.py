5d583e8020378cea51fbc1fe9e0ef7246cb89b79
liujiyangyang <1318335493@qq.com>
2019-05-07 22:28:50 +0800

    Loops/Translator

---
+++ python英文视频学习/2D Lists & Nested Loops.py
@@ -0,0 +1,26 @@
+number_grid=[
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0]
+]
+
+# print(number_grid[0][0])
+
+for row in number_grid:
+
+
+#    print(row)
+
+
+
+    for col in row:
+        print(col)
+
+
+
+
+
+
+
+
---
+++ python英文视频学习/Build a Translator.py
@@ -0,0 +1,39 @@
+# def translate(phrase):
+#     translate = ""
+#     for letter in phrase:
+#         if letter in "AEIOUaeiou":
+#             translate = translate + "g"
+#         else:
+#             translate = translate +letter
+#     return translate
+#
+# print(translate(input("ENter a phrase:")))
+
+# def translate(phrase):
+#     translation = ""
+#     for letter in phrase:
+#         if letter.lower() in "aeiou":      # 没有返回大写字母
+#             translation = translation + "g"
+#         else:
+#             translation = translation + letter
+#     return translation
+#
+# print(translate(input("Enter a phrase:")))
+
+
+# 翻译相应的单词
+
+
+def translate(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "G"
+            else:
+                translation = translation + "g"
+        else:
+            translation = translation + letter
+    return translation
+
+print(translate(input("Enter a phrase:")))
